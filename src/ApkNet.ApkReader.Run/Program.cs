6b1527657bfad0cbee882f993ef1af56ef7049aa
Selçuk Ermaya <selcukermaya@gmail.com>
2018-10-13 13:50:33 +0300

    Refactored. Renamed namespaces. Converted to .NetStandard

---
+++ src/ApkNet.ApkReader.Run/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ApkNet.ApkReader.Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string apkFilePath = @"SampleAPK\ApkReaderSample.apk";
+            const string outputLocation = @"SampleAPK\icons\";
+
+            var info = ReadApk.ReadApkFromPath(apkFilePath);
+
+            Directory.CreateDirectory(outputLocation);
+            for (var i = 0; i < info.iconFileName.Count; i++)
+            {
+
+                ExtractApkFile.ExtractFileAndSave(apkFilePath, info.iconFileName[i], @"SampleAPK\icons\", i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
